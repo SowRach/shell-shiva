8b9cb2341736ca28674b52862b3435864b932669
sowmya <sowmyaracherla3@gmail.com>
2025-06-18 03:03:10 +0530

    2nd commit

--- 04-arrays.sh
+++ 04-arrays.sh
@@ -4,12 +4,10 @@ DATE=$(date)
 
 echo "The script started executing: ${DATE}"
 
-FRUITS=("apple" "bannana" "orange")
+echo "First value: ${FRUITS[0]}"
 
-echo "First value: ${FRUTITS[0]}"
+echo "Second value: ${FRUITS[1]}"
 
-echo "Second value: ${FRUTITS[1]}"
-
-echo "Third value: ${FRUITS[2]}"
+echo "Second value: ${FRUITS[2]}"
 
 echo "All values: ${FRUITS[@]}"
