26f0cf8e88f7738bb75c3750e578920e98f8f0a9
sowmya <sowmyaracherla3@gmail.com>
2025-06-18 03:12:39 +0530

    2nd commit

--- 05-Condition.sh
+++ 05-Condition.sh
@@ -2,9 +2,11 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 100 ];
+if [ $NUMBER -gt 100 ]
 then 
     echo "Given number $NUMBER is greater than 100"
 else
     echo "Given number $NUMBER is less than 100"
 fi
+
+
