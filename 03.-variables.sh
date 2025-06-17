237309d8fb7b43dca16e678bac93cffd634a1178
sowmya <sowmyaracherla3@gmail.com>
2025-06-18 02:41:13 +0530

    2nd commit

--- 03.-variables.sh
+++ 03.-variables.sh
@@ -7,4 +7,3 @@ read -s USERNAME #the value entered above will be automatically atteched to USER
 echo "Please enter your Password:: "
 read -s PASSWORD
 
-echo "Username is: $USERNAME, Password is: $PASSWORD" # I am priting just for validation, you should not print username and passwords in scripts
